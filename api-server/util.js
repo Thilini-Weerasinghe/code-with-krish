142969e6b066973df1598c833475f7fe46d0beed
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2025-02-27 02:54:24 +0530

    Added util file to manage functions

---
+++ api-server/util.js
@@ -0,0 +1,143 @@
+function getMinNumber(num1, num2) {
+    // num1 = Number(num1);
+    // num2 = Number(num2);
+    // console.log('num1:', num1, 'num2:', num2);
+    if (isNaN(num1) || isNaN(num2)) {
+        return {
+            status: 400,
+            data: {
+                error: "both parameter shouldbe numbers"
+            }
+        };
+
+    }
+    return {
+        status: 200,
+        data: { min: Math.min(num1, num2) },
+    };
+
+};
+
+function getMaxNumber(num1, num2) {
+    // num1 = Number(num1);
+    // num2 = Number(num2);
+    // console.log('num1:', num1, 'num2:', num2);
+    // if (!/^\d+(\.\d+)?$/.test(num1) || !/^\d+(\.\d+)?$/.test(num2))
+    if (isNaN(num1) || isNaN(num2)) {
+        return {
+            status: 400,
+            data: {
+                error: "both parameter shouldbe numbers"
+            }
+        }
+
+    }
+    return {
+        status: 200,
+        data: { max: Math.max(num1, num2) },
+    }
+
+}
+
+function getAverageOfGivenNumbers(numbers) {
+    const convertedNumberArray = numbers.split(',').map(num => parseFloat(num));
+
+    if (convertedNumberArray.some(isNaN)) {
+        return {
+            status: 400,
+            data: {
+                error: "All the numbers should be valid numbers"
+            }
+        }
+
+    }
+
+    return {
+        status: 200,
+        data: convertedNumberArray.reduce((x, y) => x + y, 0) / convertedNumberArray.length,
+    }
+
+
+}
+
+function getSortedArray(numbers, type) {
+    const convertedNumberArray = numbers.split(',').map(num => parseFloat(num));
+
+
+    if (convertedNumberArray.some(isNaN)) {
+        return {
+            status: 400,
+            data: {
+                error: "All the numbers should be valid numbers"
+            }
+        }
+
+    }
+
+    const lowerCaseType = type.toLowerCase();
+
+    if (lowerCaseType === 'asc') {
+        return {
+            status: 200,
+            data: convertedNumberArray.sort((x, y) => x - y)
+
+        }
+    }
+
+    if (lowerCaseType === 'dsc') {
+        return {
+            status: 200,
+            data: convertedNumberArray.sort((x, y) => y - x)
+
+        }
+    }
+
+    return {
+        status: 400,
+        data: {
+            error: "value of type should be asc or dsc"
+        }
+    }
+
+
+}
+
+function getWordCountInArray(numbers, searchWord) {
+    const convertedArray = numbers.split(',')
+
+    //console.log(convertedArray);
+
+    let count = 0;
+
+    if (searchWord !== undefined && searchWord !== '') {
+
+        for (let word of convertedArray) {
+            // console.log(word);
+            if (word === searchWord) {
+                count++;
+            }
+        }
+
+        if (count === 0) {
+            return {
+                status: 200,
+                data: { count: `count is ${count} and we couldn't find any elemement in the array for given word ${searchWord}` }
+            }
+        }
+
+        return {
+            status: 200,
+            data: { count: `count is ${count} for the given word  ${searchWord} ` }
+        }
+    }
+    return {
+        status: 400,
+        data: {
+            error: "please provide a valid non empty word for search"
+        }
+    }
+
+
+}
+
+module.exports = { getMinNumber, getMaxNumber, getAverageOfGivenNumbers, getSortedArray, getWordCountInArray };
