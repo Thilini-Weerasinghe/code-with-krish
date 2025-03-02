30bb6cee80facf9769de2f91b5388e83159267e5
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2025-03-03 02:43:55 +0530

    added updateproductQuantity method

--- microservices-nest/cosmos/product-service/src/products/products.controller.ts
+++ microservices-nest/cosmos/product-service/src/products/products.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { Product } from './entity/product.entity';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
   async createProduct(
@@ -30,6 +30,17 @@ export class ProductsController {
     @Query('quantity') quantity: number,
   ): Promise<{ available: boolean }> {
     return this.productsService.validateStock(id, quantity);
+  }
+
+  @Patch(':id/quantity')
+  async updateProductQuantity(@Param('id') id: number, @Body() quantity: number) {
+
+    if (!quantity) {
+      throw new BadRequestException("Quantity is required");
+    }
+
+    return await this.productsService.updateProductQuantity(id, quantity);
+
   }
 }
 
--- microservices-nest/cosmos/product-service/src/products/products.service.ts
+++ microservices-nest/cosmos/product-service/src/products/products.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { error } from "console";
 import { Repository } from "typeorm";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { Product } from "./entity/product.entity";
 
 @Injectable()
 export class ProductsService {
+ 
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
@@ -34,5 +36,19 @@ export class ProductsService {
   ): Promise<{ available: boolean }> {
     const product = await this.getProductById(id);
     return { available: product.quantity >= quantity };
+  }
+
+  async updateProductQuantity(id: number, quantity: number) {
+    try {
+      const product = await this.productRepository.findOne({ where: { id } });
+      if(!product){
+        throw new error (`we couldn't find a prodcut related to this id:${id}`);
+      }
+      product.quantity = product.quantity - quantity;
+      await this.productRepository.save(product);
+    } catch (error) {
+      throw new BadRequestException(`Facing an Error while updating stock for Product ID ${id}: ${error.message}`,
+      );
+    }
   }
 }
