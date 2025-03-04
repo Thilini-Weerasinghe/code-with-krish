7b468696e3442d9e3c06acd1a11f30e1852d8fb6
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2025-03-04 14:11:29 +0530

    Added existing code into assignment

---
+++ assignment/notification-service/src/notifications/notifications.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Kafka } from 'kafkajs';
+
+@Injectable()
+export class NotificationsService implements OnModuleInit  {
+private readonly Kafka = new Kafka({brokers:[`3.0.159.213:9092`]});
+//   private readonly Kafka = new Kafka({brokers:[`localhost:9092`]});
+  private readonly producer = this.Kafka.producer();
+  private readonly consumer = this.Kafka.consumer({groupId: "thilini-notification-service"});
+   
+  async onModuleInit() {
+
+       await this.consumer.connect();
+       await this.consumeOrderConfirmed();
+        //throw new Error('Method not implemented.');
+    }
+
+    async consumeOrderConfirmed(){
+        console.log('subscription to order confirmed ===========================================>')
+        await this.consumer.subscribe({topic:`thilini.order.confirmed`});
+    
+        await this.consumer.run({
+          eachMessage: async ({message} )=> {
+            console.log(`order successfully created----------------`);
+            const {customerName,savedOrder,savedOrderItems} = JSON.parse(message.value?.toString() || "Default value if null");
+
+            console.log(customerName,savedOrder,savedOrderItems);
+
+    
+          }
+        })
+      }
+
+}
